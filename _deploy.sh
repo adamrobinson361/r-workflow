981b3b4c354eb666cc221372b3366a9cca40b32f
Adam Robinson <adamrobinson361@gmail.com>
2018-03-10 12:56:58 +0000

    Fixed directory of index output

--- _deploy.sh
+++ _deploy.sh
@@ -20,7 +20,7 @@ git clone -b gh-pages \
 
 # Copy locally built *.html files into 
 
-cp -r ../index.html ./
+cp -r index.html book-output/
 
 # Create .nojekyll file to prevent git from trying to build
 # html pages with jekyll.
